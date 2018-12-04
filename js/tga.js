47225033f40a32697749b6c701a63f32901953aa
Félix Giffard <technonerdzfg@gmail.com>
2018-12-04 15:19:32 -0500

    Team names get url

--- js/tga.js
+++ js/tga.js
@@ -2,7 +2,7 @@ var countries = new Bloodhound({
   datumTokenizer: Bloodhound.tokenizers.whitespace,
   queryTokenizer: Bloodhound.tokenizers.whitespace,
 
-  prefetch: '/api/v1/teamnames'
+  prefetch: '/api/teams/teamnames.json'
 });
 
 
@@ -13,7 +13,7 @@ $('#prefetch .typeahead').typeahead(null, {
 
 $('.typeahead').bind('typeahead:select', function(ev, suggestion) {
   console.log('Selection: ' + suggestion);
-  $.ajax({url: "/api/v1/teamnames/geturl", success: function(result){
+  $.ajax({url: "/api/teams/teamnamesgeturl.json", success: function(result){
     var urls = result;
 
   	location.href = "/team/" + urls[suggestion][0] + "/" + urls[suggestion][1];
